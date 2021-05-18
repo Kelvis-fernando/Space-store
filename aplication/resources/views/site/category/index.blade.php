83f3e9ff9b123b4b0fdc80cc407f7b3f33a772c2
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-05-18 11:48:01 -0300

    Reformulado a pagina de produtos

--- aplication/resources/views/site/category/index.blade.php
+++ aplication/resources/views/site/category/index.blade.php
@@ -3,55 +3,61 @@
 @section('title', 'Produtos')
 
 @section('content')
-<main>
-    <h2 class="text-center">Nossos Produtos</h2>
+<main id="product">
+    <h2 class="text-center text-light">Nossos Produtos</h2>
 
     <div class="content-main-margin">
         <div class="card" style="width: 18rem;">
-            <img src="{{ asset('img/empresa.png') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('img/product/produto6-min.jpg') }}" width="100%" height="200px" class="card-img-top" alt="...">
             <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button class="btn bg-success text-light">Lorem</button>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem; margin: 2rem;">
+            <img src="{{ asset('img/product/produto5-min.jpg') }}" width="100%" height="200px" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button class="btn bg-success text-light">Lorem</button>
             </div>
         </div>
 
         <div class="card" style="width: 18rem;">
-            <img src="{{ asset('img/empresa.png') }}" class="card-img-top" alt="...">
+            <img src="{{ asset('img/product/produto4-min.jpg') }}" width="100%" height="200px" class="card-img-top" alt="...">
             <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-        </div>
-
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('img/empresa.png') }}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button class="btn bg-success text-light">Lorem</button>
             </div>
         </div>
 
     </div>
+
     <div class="content-main-margin">
-    <div class="card" style="width: 18rem;">
-            <img src="{{ asset('img/empresa.png') }}" class="card-img-top" alt="...">
+        <div class="card" style="width: 18rem;">
+            <img src="{{ asset('img/product/produto3-min.jpg') }}" width="100%" height="200px" class="card-img-top" alt="...">
             <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button class="btn bg-success text-light">Lorem</button>
             </div>
         </div>
 
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('img/empresa.png') }}" class="card-img-top" alt="...">
+        <div class="card" style="width: 18rem; margin: 2rem;">
+            <img src="{{ asset('img/product/produto2-min.jpg') }}" width="100%" height="200px" class="card-img-top" alt="...">
             <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button class="btn bg-success text-light">Lorem</button>
             </div>
         </div>
 
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('img/empresa.png') }}" class="card-img-top" alt="...">
+        <div class="card" style="width: 18rem; margin: 2rem;">
+            <img src="{{ asset('img/product/produto1-min.jpg') }}" width="100%" height="200px" class="card-img-top" alt="...">
             <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <button class="btn bg-success text-light">Lorem</button>
             </div>
         </div>
+
     </div>
-    
-
 </main>
 @endsection

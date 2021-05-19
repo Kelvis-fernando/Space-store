b017bb73f1d584e306f47f8954fd11bd00479ef5
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-05-19 17:22:45 -0300

    Reformulado as rotas da aplicação seguindo a documentação do laravel 8

--- aplication/routes/web.php
+++ aplication/routes/web.php
@@ -1,16 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{HomeController, CategoryController, BlogController, ContactController};
 
 // HOME
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name('site.home');
+Route::get('/', [HomeController::class, 'index'])->name('site.home');
 
 // PRODUTOS
-Route::get('produtos', 'App\Http\Controllers\CategoryController@index')->name('site.products');
-Route::get('produtos/{slug}', 'App\Http\Controllers\CategoryController@show')->name('site.products.category');
+Route::get('produtos', [CategoryController::class, 'index'])->name('site.products');
+Route::get('produtos/{slug}', [CategoryController::class, 'show'])->name('site.products.category');
 
 // BLOG
-Route::get('blog', 'App\Http\Controllers\BlogController@index')->name('site.blog');
+Route::get('blog', [BlogController::class, 'index'])->name('site.blog');
 
 //SOBRE
 Route::get('sobre', function () {
@@ -18,5 +19,5 @@ Route::get('sobre', function () {
 })->name('site.about');
 
 //CONTATO
-Route::get('contato', 'App\Http\Controllers\ContactController@index')->name('site.contact');
-Route::post('contato', 'App\Http\Controllers\ContactController@form')->name('site.contact.form');
+Route::get('contato', [ContactController::class, 'index'])->name('site.contact');
+Route::post('contato', [ContactController::class, 'form'])->name('site.contact.form');
